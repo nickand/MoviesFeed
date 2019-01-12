604e362590b7478d8fcca9bc58748ca686ed3726
Nicolas Gonzalez <gonzalezn775@gmail.com>
2019-01-12 14:24:36 -0400

    Implementing presenter logic

---
+++ app/src/main/java/com/nickand/moviesfeed/movies/MoviesPresenter.java
@@ -0,0 +1,60 @@
+package com.nickand.moviesfeed.movies;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class MoviesPresenter implements MoviesMVP.Presenter {
+
+    private MoviesMVP.View view;
+    private MoviesMVP.Model model;
+
+    private Disposable subscription = null;
+
+    public MoviesPresenter(MoviesMVP.Model model) {
+        this.model = model;
+    }
+
+    @Override
+    public void loadData() {
+        subscription = model.result()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(new DisposableObserver<ViewModel>() {
+                @Override
+                public void onNext(ViewModel viewModel) {
+                    if (view != null) {
+                        view.updateData(viewModel);
+                    }
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    e.printStackTrace();
+                    if (view != null) {
+                        view.showSnackbar("Download movie error");
+                    }
+                }
+
+                @Override
+                public void onComplete() {
+                    if (view != null) {
+                        view.showSnackbar("Movies loaded");
+                    }
+                }
+            });
+    }
+
+    @Override
+    public void rxUnsubscribe() {
+        if (subscription != null && subscription.isDisposed()) {
+            subscription.dispose();
+        }
+    }
+
+    @Override
+    public void setView(MoviesMVP.View view) {
+        this.view = view;
+    }
+}
