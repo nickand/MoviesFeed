3cad1f196ac9219c1f92f6e26695c351203837f9
Nicolas Gonzalez <gonzalezn775@gmail.com>
2019-01-19 17:29:47 -0400

    Implementing Room - Update model classes to entities

--- app/src/main/java/com/nickand/moviesfeed/model/ViewModel.java
+++ app/src/main/java/com/nickand/moviesfeed/model/ViewModel.java
@@ -1,14 +1,42 @@
 package com.nickand.moviesfeed.model;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "movies")
 public class ViewModel {
+    @PrimaryKey
+    @ColumnInfo(name = "movieid")
+    private String id;
+    @ColumnInfo(name = "title")
     private String title;
+    @ColumnInfo(name = "country")
     private String country;
+    @ColumnInfo(name = "image")
     private String urlImage;
 
+    @Ignore
     public ViewModel(String data, String country, String urlImage) {
         title = data;
         this.country = country;
         this.urlImage = urlImage;
+    }
+
+    public ViewModel(String id, String data, String country, String urlImage) {
+        title = data;
+        this.id = id;
+        this.country = country;
+        this.urlImage = urlImage;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
