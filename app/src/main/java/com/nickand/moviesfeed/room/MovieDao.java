8b3b352086fda5cf7d2430cb019d7131921790b6
Nicolas Gonzalez <gonzalezn775@gmail.com>
2019-01-20 07:02:42 -0400

    Change liveData with Rx Observable

--- app/src/main/java/com/nickand/moviesfeed/room/MovieDao.java
+++ app/src/main/java/com/nickand/moviesfeed/room/MovieDao.java
@@ -1,6 +1,5 @@
 package com.nickand.moviesfeed.room;
 
-import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
@@ -8,6 +7,8 @@ import android.arch.persistence.room.Query;
 import com.nickand.moviesfeed.model.ViewModel;
 
 import java.util.List;
+
+import io.reactivex.Observable;
 
 import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 
@@ -17,8 +18,8 @@ public interface MovieDao {
     void save(ViewModel viewModel);
 
     @Query("SELECT * FROM movies WHERE movie_id = :movieId")
-    LiveData<ViewModel> load(int movieId);
+    Observable<ViewModel> load(int movieId);
 
     @Query("SELECT * FROM movies ORDER BY movie_id DESC")
-    LiveData<List<ViewModel>> getAllMovies();
+    Observable<List<ViewModel>> getAllMovies();
 }
